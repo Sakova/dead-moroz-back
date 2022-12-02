29f2daf1b3be44c40097a65cb6710df8b39415e3
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-12-02 15:41:50 +0300

    feat: set up registration and authorization with devise and devise-jwt

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(_resource, _opts = {})
+    render json: {
+      message: "User signed in successfully",
+      user: current_user,
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers["Authorization"].split(" ")[1], ENV["DEVISE_JWT_SECRET_KEY"]).first
+    current_user = User.find(jwt_payload["sub"])
+    if current_user
+      render json: {
+        message: "Signed out successfully"
+      }, status: :ok
+    else
+      render json: {
+        message: "User has no active session"
+      }, status: :unauthorized
+    end
+  end
+end

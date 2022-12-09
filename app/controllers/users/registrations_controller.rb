6b04305733f92c90c28df84c210779f45559c130
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-12-09 20:33:18 +0300

    feat: add new fields to user and add update user action

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,24 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  def update
+    jwt_payload = JWT.decode(request.headers["Authorization"].split(" ")[1], ENV["DEVISE_JWT_SECRET_KEY"]).first
+    current_user = User.find(jwt_payload["sub"])
+
+    if current_user.update(user_params)
+      render json: {
+        message: "Update successfully",
+        user: current_user,
+        params: account_update_params
+      }, status: :ok
+    else
+      render json: {
+        message: "Update failed",
+        errors: resource.errors.full_messages,
+      }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def respond_with(resource, _options = {})
@@ -13,8 +31,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render json: {
         message: "user could not be signed up",
         errors: resource.errors.full_messages,
-        status: :unprocessable_entity
-      }
+      }, status: :unprocessable_entity
     end
+  end
+
+  def user_params
+    params.permit(:name, :surname, :age, :avatar, :street, :house, :floor, :email)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_174222) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_172859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_174222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
+    t.string "name"
+    t.string "surname"
+    t.integer "age"
+    t.string "avatar"
+    t.string "street"
+    t.integer "house"
+    t.integer "floor"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

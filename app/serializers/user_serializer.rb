e8cee43c0a1adaf4de919e6e95c25d7649fddd2e
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-03 13:17:44 +0300

    feat: add necessary attributes to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :jti, :name, :surname, :role, :email, :age, :items, :address, :avatar
+  attributes :id, :jti, :name, :surname, :role, :email, :age, :items, :address, :avatar, :total_pages
+
+  has_many :assessments, if: :allow_access?
+  has_many :feedbacks, if: :allow_access?
+  has_many :gifts, if: :allow_access?
 
   def address
     address = self.object.address
@@ -9,5 +13,17 @@ class UserSerializer < ActiveModel::Serializer
 
   def avatar
     self.object.get_avatar_url if self.object.avatar.present?
+  end
+
+  def total_pages
+    User.all.page(1).total_pages
+  end
+
+  def allow_access?
+    if current_user.elf? || current_user.dead?
+      true
+    else
+      false
+    end
   end
 end

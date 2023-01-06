ccfb093b5618f36d2187e197f0f4b4662ac27c88
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-06 11:30:36 +0300

    fix: change logic of profile translation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,10 +10,10 @@ class Api::V1::UsersController < ApplicationController
   def translate
     result = Rails.cache.fetch("user/#{params[:user]}", expires_in: 1.day) do
       user = User.find(params[:user])
-      gifts = user.gifts.map{|gift| gift.description}
+      gifts = user.gifts.map{|gift| gift.description}.join("___")
 
       translated_gifts = ProfileTranslate.translate(gifts)
-      translated_items = ProfileTranslate.translate(user.items)
+      translated_items = ProfileTranslate.translate(user.items.join("___"))
 
       {items: translated_items, gifts: translated_gifts}
     end
